0245a77e1976f05ec9b4052a0f24d790b8519a36
HaoYi <karenhaoyitu@gmail.com>
2019-04-08 18:01:54 +0800

    Updates

---
+++ precache-manifest.f9ae54fb1191918c905d978fd599d378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "487a22954339da0a76e3",
+    "url": "/static/js/main.1c7c6d04.chunk.js"
+  },
+  {
+    "revision": "8ac4db4d80393c0a4ae2",
+    "url": "/static/js/2.c301146f.chunk.js"
+  },
+  {
+    "revision": "487a22954339da0a76e3",
+    "url": "/static/css/main.93b9bade.chunk.css"
+  },
+  {
+    "revision": "94be8bf0605d70336f37948b432aad63",
+    "url": "/index.html"
+  }
+];
