85edfe73097669a6aa89a5002835fa2c016629ea
HaoYi <karenhaoyitu@gmail.com>
2019-04-14 18:10:52 +0800

    Updates

--- precache-manifest.d358b4e3c3e879b09c6e38c7a13ae809.js
+++ precache-manifest.d358b4e3c3e879b09c6e38c7a13ae809.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Homie/static/js/runtime~main.ad1b8cca.js"
   },
   {
-    "revision": "bd76a5a96714978645d4",
-    "url": "/Homie/static/js/main.cc1fadea.chunk.js"
+    "revision": "8e70668d238dfef39a5f",
+    "url": "/Homie/static/js/main.b31ffa7c.chunk.js"
   },
   {
     "revision": "cae54ba8f9915d263765",
     "url": "/Homie/static/js/2.fe16d2ea.chunk.js"
   },
   {
-    "revision": "bd76a5a96714978645d4",
-    "url": "/Homie/static/css/main.a5cbda84.chunk.css"
+    "revision": "8e70668d238dfef39a5f",
+    "url": "/Homie/static/css/main.c3a25e15.chunk.css"
   },
   {
-    "revision": "561d1f52e4f21df2b7b8d23a41081726",
+    "revision": "c48d70d0bad8839c0677728bff2c88d2",
     "url": "/Homie/index.html"
   }
 ];
