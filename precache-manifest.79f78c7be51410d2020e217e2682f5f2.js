35ee1b2d6711bdf17194c8334e85fedd5776dd15
HaoYi <karenhaoyitu@gmail.com>
2019-05-05 16:22:41 +0800

    Updates

---
+++ precache-manifest.79f78c7be51410d2020e217e2682f5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f770c373f2725015a53bde7de7768b",
+    "url": "/Homie/index.html"
+  },
+  {
+    "revision": "f79d4a44e85fe00e31e4",
+    "url": "/Homie/static/css/main.8bf3efa9.chunk.css"
+  },
+  {
+    "revision": "3352693cba7dd8a3e68b",
+    "url": "/Homie/static/js/2.3d78ea9d.chunk.js"
+  },
+  {
+    "revision": "f79d4a44e85fe00e31e4",
+    "url": "/Homie/static/js/main.bc99c358.chunk.js"
+  },
+  {
+    "revision": "35017bec007661a25259",
+    "url": "/Homie/static/js/runtime~main.ad1b8cca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/Homie/precache-manifest.1d211d7e9bd2963e7e5d60b1730c7eba.js"
+  "/Homie/precache-manifest.79f78c7be51410d2020e217e2682f5f2.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Homie/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Homie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
