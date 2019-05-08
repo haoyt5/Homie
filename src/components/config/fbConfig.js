059f65a850aa3634369cca48c5bf51feeeb57245
HaoYi <karenhaoyitu@gmail.com>
2019-05-08 09:20:52 +0800

    update

--- src/components/config/fbConfig.js
+++ src/components/config/fbConfig.js
@@ -9,8 +9,7 @@ var config = {
     authDomain: "homie-2019.firebaseapp.com",
     databaseURL: "https://homie-2019.firebaseio.com",
     projectId: "homie-2019",
-    storageBucket: "homie-2019.appspot.com",
-    messagingSenderId: "769792607981"
+    storageBucket: "homie-2019.appspot.com"
 }
 firebase.initializeApp(config);
 const storage = firebase.storage();
