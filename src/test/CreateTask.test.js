7648651b5a391be44b3be4dbdaf78862285543d1
HaoYi <karenhaoyitu@gmail.com>
2019-05-14 23:37:49 +0800

    refactor the shallowwrapper and attribute finding code

--- src/test/CreateTask.test.js
+++ src/test/CreateTask.test.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import Enzyme,{ shallow, render } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
-import { storeFactory } from './testUtilities'
-import CreactTask from '../components/tasks/CreateTask'
-
+import Enzyme,{ shallow, render } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { storeFactory } from './testUtilities';
+import CreateTask from '../components/tasks/CreateTask';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup =(initState={}) =>{
+    const store = storeFactory(initState);
+    return shallow(<CreateTask store={store} />);
+}
+
+const findTestAttr = (wrapper, value) =>{
+    return wrapper.dive().find(`[data-test="${value}"]`);
+}
+
+describe('render',() =>{
+    describe('nothing has not been submitted', ()=>{
+        test('renders component without error',()=>{
+            const wrapper = setup();
+            const createTaskComponent = findTestAttr(wrapper,'component-create-task');
+            expect(createTaskComponent.length).toBe(1);
+        })
+        test('renders toggle input',()=>{
+            const wrapper = setup();
+            const checkbox = findTestAttr(wrapper,'createtask-checkbox');
+            expect(checkbox.length).toBe(1);
+            
+        })
+        test('renders submit button',()=>{
+            const wrapper = setup();
+            const button = findTestAttr(wrapper,'createtask-button');
+            expect(button.length).toBe(1);
+        })
+        test('checkbox start at false',(initState={})=>{
+            
+        })
+    })
+})
+describe('update state',() =>{
+    // test('clicking checkbox toggle the check display',()=>{
+            
+    // })
+})
 // const setup = (initState={}) => {
 //     const store = storeFactory(initState)
 //     const wrapper = shallow(<CreactTask store={store}/>);
@@ -20,35 +56,3 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 //     return shallow(<CreactTask />)
 // }
 // setup();
-
-describe('render',() =>{
-    describe('nothing has not been submitted', ()=>{
-        test('renders component without error',(initState={})=>{
-            const store = storeFactory(initState)
-            const wrapper = shallow(<CreactTask store={store} />)
-            const createTaskComponent = wrapper.dive().find('[data-test="component-create-task"]')
-            expect(createTaskComponent.length).toBe(1);
-        })
-        test('renders toggle input',(initState={})=>{
-            const store = storeFactory(initState)
-            const wrapper = shallow(<CreactTask store={store} />)
-            const checkbox = wrapper.dive().find('[data-test="createtask-checkbox"]')
-            expect(checkbox.length).toBe(1);
-            
-        })
-        test('renders submit button',(initState={})=>{
-            const store = storeFactory(initState)
-            const wrapper = shallow(<CreactTask store={store} />)
-            const button = wrapper.dive().find('[data-test="createtask-button"]')
-            expect(button.length).toBe(1);
-        })
-        test('checkbox start at false',(initState={})=>{
-            
-        })
-    })
-})
-describe('update state',() =>{
-    // test('clicking checkbox toggle the check display',()=>{
-            
-    // })
-})
